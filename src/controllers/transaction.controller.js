02efa4fea5806cad17d05ad2c2ebe8baa3b8f82d
oliveiratullio <tullio_sfmg2@hotmail.com>
2023-04-25 15:44:11 -0300

    dayjs

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,12 +1,20 @@
 import { db } from "../database/database.connection.js";
+import dayjs from "dayjs";
 
 export async function transaction(req, res) {
-    try{
-        const session = res.locals.session;
-        await db.collection('transactions').insertOne({...req.body, value: Number(req.body.value), userId: session.userId, date: dayjs().format()});
-        res.sendStatus(201);
-    } catch(error){
-        res.status(500).send(error.message);
+    const { description, value, type } = req.body
+    const { session, user } = res.locals
+    try {
+        db.collection("transactions").insertOne({
+            description,
+            value: Number(value.toFixed(2)),
+            type,
+            userId: session.userId,
+            date: dayjs().format("DD/MM")
+        })
+        return res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
     }
 }
 export async function listTransaction(req, res){
