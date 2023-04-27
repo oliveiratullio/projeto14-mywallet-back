c989e42704a377a732431ad3065e7b53eb01229f
oliveiratullio <oliveiratulliof@gmail.com>
2023-04-27 11:19:55 -0300

    "chore: adjust code for deploy on render"

--- src/app.js
+++ src/app.js
@@ -10,4 +10,6 @@ app.use(express.json())
 app.use(router)
 
 const PORT = 5000
-app.listen(process.env.PORT, () => console.log("Servidor rodando na porta " + process.env.PORT))
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
